a26ff135eff53753d9fee02eae1ad661602e45ca
r00sha <r00sha@users.noreply.github.com>
2020-11-23 23:07:25 +0700

    jadi error routingnya wkwkwk maap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./main/home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-  },  {
+  },
+  {
     path: 'pakaian',
     loadChildren: () => import('./pakaian/pakaian.module').then( m => m.PakaianPageModule)
   },
@@ -18,6 +19,19 @@ const routes: Routes = [
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
   },
+  {
+    path: 'scoreboard',
+    loadChildren: () => import('./main/scoreboard/scoreboard.module').then(m => m.ScoreboardPageModule)
+  },
+  {
+    path: 'user',
+    loadChildren: () => import('./main/user/user.module').then(m => m.UserPageModule)
+  },
+  {
+    path: 'main',
+    loadChildren: () => import('./main/main.module').then( m => m.MainPageModule)
+  },
+
 
 ];
 
