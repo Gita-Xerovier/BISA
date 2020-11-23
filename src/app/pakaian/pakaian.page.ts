396c11e66593361b06cab1a875dfd1cf3db0e427
Doremu <62300995+Doremu@users.noreply.github.com>
2020-11-23 21:21:43 +0700

    buat activity pakaian

---
+++ src/app/pakaian/pakaian.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pakaian',
+  templateUrl: './pakaian.page.html',
+  styleUrls: ['./pakaian.page.scss'],
+})
+export class PakaianPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
